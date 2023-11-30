8081e8996d182f9aef3c7340b07ebddf780bc9ff
Aida-Meculescu <aida.meculescu@gmail.com>
2023-11-30 23:12:06 +0200

    ongoing - SignupPage

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const path = require('path') // is a build in package
 const express = require('express')
 
 const authRouters = require('./routes/auth.routes')
 
 const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views')) // creat an absolute path that is reconize on every operating systems
 
 app.use(authRouters)
 
